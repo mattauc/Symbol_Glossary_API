61c2166b649c3ef162735fcc8b24fc76868a0c84
James <28093344+Itsnotjames@users.noreply.github.com>
2024-07-21 19:53:08 +1200

    first attempt

--- app/model.py
+++ app/model.py
@@ -1,2 +1,100 @@
-import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch
+
+train_data = datasets.MNIST(
+    root = 'data',
+    train = True,
+    transform = ToTensor(),
+    download = True
+)
+
+test_data = datasets.MNIST(
+    root = 'data',
+    train = False,
+    transform = ToTensor(),
+    download = True
+)
+
+print(train_data)
+print(test_data)
+print(train_data.data)
+print(train_data.data.shape)
+
+loaders = {
+    'train' : DataLoader(train_data,
+                         batch_size=100,
+                         shuffle=True,
+                         num_workers=1),
+
+    'test' : DataLoader(test_data,
+                         batch_size=100,
+                         shuffle=True,
+                         num_workers=1)
+}
+
+class CNN(nn.Module):
+
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        convChannels = 20
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, convChannels, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(convChannels*4*4, 50)
+        self.fc2 = nn.Linear(50, 10) # 10 will be different with our dataset
+        
+        def forward(self, x):
+            x = F.relu(F.max_pool2d(self.conv1(x), 2))
+            x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+            x = x.view(-1, convChannels*4*4)
+            x = F.relu(self.fc1(x))
+            x = F.dropout(x, tranining=self.training)
+            x = self.fc2(x)
+
+            return F.softmax(x)
+        
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')      
+model = CNN().to(device)  
+
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+loss_fn = nn.CrossEntropyLoss()
+
+def train(epoch):
+    model.train()
+
+    for batch_idx, (data, target) in enumerate(loaders['train']):
+        data, target = data.to(device, target.to(device))
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_fn(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_inx % 20 == 0:
+            print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(loaders["train"].dataset)} ({100. * batch_idx / len(loaders["train"]):.0f}%)]\t{loss.item():.6f}')
+            
+def test():
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for data, target in loaders['test']:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += loss_fn(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct+= pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(loaders['test'].dataset)
+    print(f'\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(loaders["test"].dataset)} ({100. * correct / len(loaders["test"].dataset):.0f}%\n)')
+    
+for epoch in range(1, 11):
+    train(epoch)
+    test()    
